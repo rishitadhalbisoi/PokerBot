46092f262d202a27e1006cd27305277bb803da47
rishitadhalbisoi <79031603+rishitadhalbisoi@users.noreply.github.com>
2024-02-29 17:24:03 -0800

    Update SevenCardPoker.java

--- src/SevenCardPoker.java
+++ src/SevenCardPoker.java
@@ -1,11 +1,22 @@
 public class SevenCardPoker {
     public static int getValue(Card[] h7) {
         Card[] h5 = new Card[5];
-        for (int i = 0; i < (1 << 7); i++) {
-            if (Integer.bitCount(i) == 5) {
-                
-
+        for (int mask = 0; mask < (1 << 7); i++) {
+            if (Integer.bitCount(mask) == 5) {
+                h5 = getFive(mask, h7);
             }
         }
     }
+    private static Card[] getFive(int mask, Card[] h7){
+        Card[] h5 = new Card[5];
+        int index = 0;
+        for (int i = 0; i < 7; i++){
+            if ((mask & (1<<i)) == 1){
+                h5[index++] = h7[i];
+            }
+        }
+        return h5;
+    }
 }
+
+
